47ddb19ad55507c136009d00f1ff48282b77f951
Алексей Никитин <alexeybezhnar@gmail.com>
2019-10-06 11:21:28 +0300

    Rename class

---
+++ AppTestingSolution/AppTesting/Models/TestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTesting.Models
+{
+    public class TestModel
+    {
+        public string TestName { get; set; }
+
+        public List<QuestionModel> QuestionList { get; set; }
+
+    }
+}
