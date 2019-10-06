81d4dfba78ddb8730d1df6b3bca94ecabe5ab080
Алексей Никитин <alexeybezhnar@gmail.com>
2019-10-06 10:33:20 +0300

    Добавился класс

---
+++ AppTestingSolution/AppTesting/Models/AnswerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTesting.Models
+{
+    public class AnswerModel
+    {
+        public string Answer { get; set; }
+
+        public bool Right { get; set; }
+    }
+}
--- AppTestingSolution/AppTesting/Program.cs
+++ AppTestingSolution/AppTesting/Program.cs
@@ -11,7 +11,11 @@ namespace AppTesting
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp().Start(AppMain, args);
+        public static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.WriteLine(args);
+            BuildAvaloniaApp().Start(AppMain, args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
