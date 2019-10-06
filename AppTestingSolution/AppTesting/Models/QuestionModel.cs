f303083aa0397ee4d7505fea322e2bf0c706f3b3
Алексей Никитин <alexeybezhnar@gmail.com>
2019-10-06 11:16:49 +0300

    Class testdata/question/answer

---
+++ AppTestingSolution/AppTesting/Models/QuestionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTesting.Models
+{
+    public class QuestionModel
+    {
+        public string Question { get; set; }
+
+        public List<AnswerModel> AnswerList { get; set; }
+    }
+}
---
+++ AppTestingSolution/AppTesting/Models/TestData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTesting.Models
+{
+    public class TestData
+    {
+        public string TestName{ get; set; }
+
+        public List<TestData> QuestionList { get; set; }
+
+    }
+}
